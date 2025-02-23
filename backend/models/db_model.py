9522a7662e9d43ae5de106ffb5e35725176c4159
Varya Anis <vranisimova@edu.hse.ru>
2025-02-23 11:22:01 +0300

    in the middle of writing paper_data db sync

--- backend/models/db_model.py
+++ backend/models/db_model.py
@@ -51,6 +51,15 @@ class PaperDataCache(Base):
     timestamp = Column(DateTime, default=datetime.now, nullable=False)
 
 
+class AssetsTable(Base):
+    __tablename__ = "assets_table"
+    uid = Column(String, primary_key=True, index=True)
+    name = Column(String)
+    figi = Column(String)
+    ticker = Column(String, unique=True, index=True)
+    class_code = Column(String)
+
+
 class DividendsCache(Base):
     __tablename__ = "dividends_cache"
     figi = Column(String, primary_key=True)
