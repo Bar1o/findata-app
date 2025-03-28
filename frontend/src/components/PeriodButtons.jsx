e784d2fca300aeaaa3283213749533d847676f02
Varya Anis <vranisimova@edu.hse.ru>
2025-03-28 18:03:32 +0300

    ichimoku fixed and buttons

--- frontend/src/components/PeriodButtons.jsx
+++ frontend/src/components/PeriodButtons.jsx
@@ -1,49 +1,15 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-import { createChart } from "lightweight-charts";
-import PropTypes, { number } from "prop-types";
+import React from "react";
+import PropTypes from "prop-types";
 import { Button } from "@gravity-ui/uikit";
-import { FetchDataByPeriod, IchimokuData } from "./FetchData";
 
-const PeriodButtons = ({ ticker, setChartData }) => {
+const PeriodButtons = ({ period, setPeriod }) => {
   const periods = ["D", "3D", "W", "M", "3M", "Y"];
 
-  const [activePeriod, setActivePeriod] = useState("W");
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  const handleClickedPeriod = useCallback(
-    async (period) => {
-      setActivePeriod(period);
-      setLoading(true);
-      setError(null);
-
-      try {
-        const rawData = await FetchDataByPeriod({ ticker, period });
-        const transformedData = IchimokuData(rawData);
-        setChartData(transformedData);
-      } catch (err) {
-        console.error(`Error fetching data for period "${period}":`, err);
-        setError("Failed to fetch data. Please try again.");
-        setChartData([]); // Optionally clear existing chart data on error
-      } finally {
-        setLoading(false);
-      }
-    },
-    [ticker, loading, setChartData]
-  );
-
   return (
-    <div className="text-sm flex flex-row gap-2 p-2 sm:gap-4 md:gap-5 ">
-      {periods.map((period) => (
-        <Button
-          key={period}
-          selected={activePeriod === period}
-          // view="outlined-info"
-          size="l"
-          width="small"
-          onClick={() => handleClickedPeriod(period)}
-        >
-          {period}
+    <div className="text-sm flex flex-row gap-2 p-2 sm:gap-4 md:gap-5">
+      {periods.map((p) => (
+        <Button key={p} selected={period === p} size="l" width="small" onClick={() => setPeriod(p)}>
+          {p}
         </Button>
       ))}
     </div>
@@ -51,8 +17,8 @@ const PeriodButtons = ({ ticker, setChartData }) => {
 };
 
 PeriodButtons.propTypes = {
-  ticker: PropTypes.string.isRequired,
-  setChartData: PropTypes.func.isRequired,
+  period: PropTypes.string.isRequired,
+  setPeriod: PropTypes.func.isRequired,
 };
 
 export default PeriodButtons;
