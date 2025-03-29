64b931594f429022f46e4a221c816fee87f8c487
Varya Anis <vranisimova@edu.hse.ru>
2025-03-29 12:47:56 +0300

    all

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,12 +12,5 @@ export default defineConfig({
   },
   server: {
     port: 3001,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3300/",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
   },
 });
